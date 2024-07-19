7be35fd97e882559aeda9fa83e140a1335539465
MinGyu-home <alsrb2277@naver.com>
2024-07-19 18:23:12 +0900

    2-3. useRef 실습 '작성완료' 클릭 시 입력 폼 초기화

--- react_study/src/ch_03/InputText.jsx
+++ react_study/src/ch_03/InputText.jsx
@@ -1,16 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 
 function InputText() {
     const [text, setText] = useState("");
+    const textRef = useRef();
 
     const handleOnChange = (e) => {
         setText(e.target.value);
+    };
+    const handleOnClick = () => {
+        textRef.current.value = "";
     }
 
     return (
         <div>
-            <input onChange={handleOnChange} />
-            <div>{text}</div>
+            <input ref={textRef} value={text} onChange={handleOnChange} />
+            <button onClick={handleOnClick}>작성 완료</button>
         </div>
     );
 }
--- react_study/src/index.js
+++ react_study/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter2 from './ch_03/Counter2.jsx';
+// import Counter2 from './ch_03/Counter2.jsx';
+import InputText from './ch_03/InputText.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Counter2></Counter2>
+  <InputText></InputText>
 );
