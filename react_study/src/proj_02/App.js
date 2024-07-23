93b38eae9ce5f06e92b36ffa855fda5da7046193
MinGyu-home <alsrb2277@naver.com>
2024-07-23 22:30:54 +0900

    Project2 - TodoList 만들기(2) : Create 과제1

--- react_study/src/proj_02/App.js
+++ react_study/src/proj_02/App.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, {useState,useRef} from "react";
 import "./App.css";
 import Header from "./Header";
 import TodoEditor from "./TodoEditor";
 import TodoList from "./TodoList";
 
+const mockTodo = [
+    {
+        id: 0,
+        isDone: false,
+        content: "리액트 공부",
+        createDate: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "팀 구성",
+        createDate: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "프로젝트 계획서 작성",
+        createDate: new Date().getTime(),
+    },
+]
+
 function App() {
+    const [todo, setTodo] = useState(mockTodo);
+    const idRef = useRef(3);
+
+    function onCreate(content) {
+        const newItem = {
+            id: idRef.current,
+            isDone: false,
+            content,
+            createDate: new Date().getTime(),
+        };
+        setTodo([newItem, ...todo])
+        idRef.current += 1;
+    }
     return (
         <div className="App">
             <Header></Header>
-            <TodoEditor></TodoEditor>
+            <TodoEditor onCreate={onCreate}></TodoEditor>
             <TodoList></TodoList>
         </div>
     );
--- react_study/src/proj_02/TodoEditor.jsx
+++ react_study/src/proj_02/TodoEditor.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import "./TodoEditor.css";
 
-function TodoEditor() {
+function TodoEditor({onCreate}) {
+    const [content, setContent] = useState("");
+    const textRef = useRef();
+
+    function onChangeContent(e) {
+        setContent(e.target.value);
+    };
+    function onSubmit() {
+        if (content.length <= 0) {
+            textRef.current.focus();
+        } else {
+            onCreate(content);
+        }
+    }
+
     return (
         <div className="TodoEditor">
                 <h3>새로운 Todo 작성하기</h3>
                 <div className="editor_wrapper">
-                    <input placeholder="새로운 Todo..." />
-                    <button>추가</button>
+                    <input ref={textRef} value={content} onChange={onChangeContent} placeholder="새로운 Todo..." />
+                    <button onClick={onSubmit}>추가</button>
                 </div>
         </div>
     );
