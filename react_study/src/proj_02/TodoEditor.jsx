f4d0eb93d3eff2bd341e255af7f6e42ff88fb8ef
MinGyu-home <alsrb2277@naver.com>
2024-07-24 16:02:58 +0900

    Project2 - Todo List 만들기(3) : Create 과제2, 3

--- react_study/src/proj_02/TodoEditor.jsx
+++ react_study/src/proj_02/TodoEditor.jsx
@@ -8,19 +8,27 @@ function TodoEditor({onCreate}) {
     function onChangeContent(e) {
         setContent(e.target.value);
     };
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter'){
+            onSubmit();
+        }
+    };
+    
     function onSubmit() {
         if (content.length <= 0) {
             textRef.current.focus();
         } else {
             onCreate(content);
+            setContent("");
         }
-    }
+    };
 
     return (
         <div className="TodoEditor">
                 <h3>새로운 Todo 작성하기</h3>
                 <div className="editor_wrapper">
-                    <input ref={textRef} value={content} onChange={onChangeContent} placeholder="새로운 Todo..." />
+                    <input ref={textRef} value={content} onChange={onChangeContent} onKeyDown={handleKeyDown} placeholder="새로운 Todo..." />
                     <button onClick={onSubmit}>추가</button>
                 </div>
         </div>
